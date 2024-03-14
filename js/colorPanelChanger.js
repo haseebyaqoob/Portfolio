c2aa8ffbc6e3dd0f8f9b181bd3a9736e26614d83
Haseeb Yaqoob <haseeb.yaqoob201@gmail.com>
2024-03-15 01:36:01 +0500

    Create colorPanelChanger.js

---
+++ js/colorPanelChanger.js
@@ -0,0 +1,12 @@
+$(function () {
+  $(".color-panel").on("click", function (e) {
+    e.preventDefault();
+    $(".color-changer").toggleClass("color-changer-active");
+  });
+  $(".colors a").on("click", function (e) {
+    e.preventDefault();
+    var attr = $(this).attr("title");
+    console.log(attr);
+    $("head").append('<link rel="stylesheet" href="css/' + attr + '.css">');
+  });
+});
