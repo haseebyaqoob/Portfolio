f42f8c9ad9660332a63b452f3c9b9f7711bbcf2d
Haseeb Yaqoob <haseeb.yaqoob201@gmail.com>
2024-03-15 01:48:10 +0500

    Update menuBarAnimation.js

--- js/menuBarAnimation.js
+++ js/menuBarAnimation.js
@@ -1,8 +1,9 @@
 $(function () {
   $(".menubar").on("click", function () {
-    gsap.to("#navigation-content", 0.6, { y: 0 });
+    gsap.to("#navigation-content", { duration: 0.6, y: 0, ease: "power2.out" });
   });
+
   $(".navigation-close").on("click", function () {
-    gsap.to("#navigation-content", 0.6, { y: "-100%" });
+    gsap.to("#navigation-content", { duration: 0.6, y: "-100%", ease: "power2.out" });
   });
 });
